eae05ae1b71da7a7ec7296261533c326b28c3e52
djmovemove <116862172+djmovemove@users.noreply.github.com>
2025-04-14 17:08:35 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MBTITest from "./MBTITest";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <MBTITest />
+    </div>
+  );
+}
+
+export default App;
